a2549be18bb722586a840c8cc7de2469d0c5ef56
double J, T <50047351+Powering111@users.noreply.github.com>
2021-08-18 21:17:12 +0900

    help me

--- 얼굴 검출.py
+++ 얼굴 검출.py
@@ -1,12 +1,6 @@
 import cv2, numpy as np
- 
+from face_utils import *
 
-def preprocess():
-    image=cv2.imread('img.jpg',cv2.IMREAD_COLOR)
-    if image is None : return None, None
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.equalizeHist(gray)
-    return image,gray
 
 face_cascade = cv2.CascadeClassifier("haar/haarcascade_frontalface_alt2.xml")
 eye_cascade = cv2.CascadeClassifier("haar/haarcascade_eye.xml")
@@ -20,13 +14,22 @@ if faces.any():
     print(faces[0])
     x,y,w,h=faces[0]
     face_image = image[y:y+h,x:x+w]
-    eyes = eye_cascade.detectMultiScale(face_image)
+    eyes = eye_cascade.detectMultiScale(face_image,1.15,7,0,(25,20))
     
     if len(eyes) == 2:
-        for ex,ey,ew,eh in eyes:
-            center= (x+ex+ew//2, y+ey+eh//2)
-            cv2.circle(image,center,10,(0,255,0),2)
+        face_center=(x+w//2,y+h//2)
+        face_center_f=(float(x+w//2),float(y+h//2))
+        eye_centers=[(x+ex+ew//2,y+ey+eh//2) for ex,ey,ew,eh in eyes]
+        corr_image, corr_center = correct_image(image,face_center_f,eye_centers)
+        rois=detect_object(face_center,faces[0])
 
+        cv2.rectangle(corr_image,rois[0],(255,0,255),2)
+        cv2.rectangle(corr_image,rois[1],(255,0,255),2)
+        cv2.rectangle(corr_image,rois[2],(255,0,0),2)
+        cv2.circle(corr_image,tuple(corr_center[0]),5,(0,255,0),2)
+        cv2.circle(corr_image,tuple(corr_center[1]),5,(0,255,0),2)
+        cv2.circle(corr_image,face_center,3,(0,0,255),2)
+        cv2.imshow("yeah",corr_image)
     else:
         print("눈이 없음")
 
